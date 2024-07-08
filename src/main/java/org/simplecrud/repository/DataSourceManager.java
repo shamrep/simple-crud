e438ab6e3bfda80a806ef5458120ee02cce2d199
shamrep <shamrep@gmail.com>
2024-07-08 13:43:23 +0300

    fix: change properties

--- src/main/java/org/simplecrud/repository/DataSourceManager.java
+++ src/main/java/org/simplecrud/repository/DataSourceManager.java
@@ -3,15 +3,25 @@ package org.simplecrud.repository;
 import org.postgresql.ds.PGSimpleDataSource;
 
 import javax.sql.DataSource;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
 
 public class DataSourceManager {
 
     private static final PGSimpleDataSource dataSource = new PGSimpleDataSource();
+    private static Properties properties = new Properties();
 
     static {
-        dataSource.setURL("jdbc:postgresql://localhost:5432/quizzes");
-        dataSource.setUser("postgres");
-        dataSource.setPassword("1234");
+        try (FileInputStream fis = new FileInputStream("src/main/resources/application.properties")) {
+            properties.load(fis);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        dataSource.setURL(properties.getProperty("db.url"));
+        dataSource.setUser(properties.getProperty("db.username"));
+        dataSource.setPassword(properties.getProperty("db.password"));
     }
 
     public static DataSource getDataSource() {
